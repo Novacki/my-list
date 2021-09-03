a4ad9cde5d34328b6a49146beedd92d3d1e1dcd5
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2021-09-03 01:19:36 -0300

    adding entity framework with identity

--- src/Server/Services/Identity/Identity.API/Startup.cs
+++ src/Server/Services/Identity/Identity.API/Startup.cs
@@ -1,16 +1,11 @@
+using Identity.infrastructure.Data.Contexts.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Identity.API
 {
@@ -32,6 +27,9 @@ namespace Identity.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity.API", Version = "v1" });
             });
+
+            services.AddDbContext<IdentityDataContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/Server/Services/Identity/Identity.infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Identity.infrastructure
-{
-    public class Class1
-    {
-    }
-}
